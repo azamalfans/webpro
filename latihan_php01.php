5d36f8336e5da519a775da5f5231f22c1951218b
Ibnu Azam A <44658303+azamalfans@users.noreply.github.com>
2019-09-13 08:55:10 +0700

    Update latihan_php01.php

--- latihan_php01.php
+++ latihan_php01.php
@@ -1,3 +1,25 @@
 <?php
+echo "<h1>Hello World</h1>";
 
+//$angkaBagus = 10;
+//echo $angkaBagus;
+//echo "Angka bagus adalah ".$angkaBagus
+
+//$nilai = 70;
+// if($nilai > 80)
+// {
+//     echo "Good Job!";
+// }
+// elseif($nilai > 75)
+// {
+//     echo "Belajar Lagi yaa...";
+// }
+// else{
+//     echo "Belajar lagi ya...";
+// }
+
+for($i=1 ; $i<=10 ; $i++)
+{
+    echo $i."<br/>";
+}
 ?>
