951de8614306e8c0e3a8b1a105f9146cb3494171
Ibnu Azam A <azam.alfanibnu@gmail.com>
2019-10-04 08:43:33 +0700

    sgvsas

---
+++ LatihanDasar/latihan_php04.php
@@ -0,0 +1,9 @@
+<form action="login.php" method="post">
+Username<br />
+<input type="text" name="username" />
+<br />
+Password<br />
+<input type="password" name="pass" />
+<br />
+<input type="submit" />
+</form>
